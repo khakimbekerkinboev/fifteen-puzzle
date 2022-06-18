f855dcd05871e78620d17baf59a1e16cea607b0b
khakimbekerkinboev <erkinboevkhakimbek@gmail.com>
2022-06-18 11:23:30 +0500

    feat: change the width of each number to relative for mobile screen size

--- fifteen-puzzle/main.js
+++ fifteen-puzzle/main.js
@@ -1,2 +1,18 @@
 const restart = document.querySelector('.restart')
 const pause = document.querySelector('.pause')
+const cells = document.querySelectorAll('.single-cell')
+const items = document.querySelectorAll('.single-item')
+
+//randomize indexes of any array by length
+function arrayRandom(length) {
+  let arr = []
+  while (arr.length < length) {
+    const newNumber = Math.floor(Math.random() * length)
+    if (!arr.includes(newNumber)) {
+      arr.push(newNumber)
+    }
+  }
+  return arr
+}
+
+console.log(arrayRandom(15))
